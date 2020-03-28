47ab58937373b2849faf214e3f421dd7205579e4
Tran Cao Trung <caotrung4691@gmail.com>
2020-03-28 10:40:37 +0700

    Updates

---
+++ precache-manifest.b946d1c9f8ce6bd101963e4927d7b2d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e67a7c898bb84e9f2187cea44392c8",
+    "url": "/post-comment-app/index.html"
+  },
+  {
+    "revision": "5551e7ea8cffcb1eda46",
+    "url": "/post-comment-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bb2e208a3b57c5241c8",
+    "url": "/post-comment-app/static/js/2.8bb2c2ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/post-comment-app/static/js/2.8bb2c2ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5551e7ea8cffcb1eda46",
+    "url": "/post-comment-app/static/js/main.9ca1037d.chunk.js"
+  },
+  {
+    "revision": "eca5b35fc079035c0a4e",
+    "url": "/post-comment-app/static/js/runtime-main.fe59534c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/post-comment-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
